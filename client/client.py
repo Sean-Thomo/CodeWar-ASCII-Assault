004f2032df815a98775601b8f2737ec71033442f
Sean-Thomo <seansthomo@gmail.com>
2023-08-24 18:57:42 +0200

    CHORE(client/server): run send and receive threads for clients

--- client/client.py
+++ client/client.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 HEADER = 64
 PORT = 5050
@@ -10,17 +11,36 @@ ADDR = (SERVER, PORT)
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect(ADDR)
+nickname = input("Choose a nickname: ")
 
-def send(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER-len(send_length))
-    client.send(send_length)
-    client.send(message)
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode(FORMAT)
+            if message == "NICK":
+                client.send(nickname.encode(FORMAT))
+            else:
+                print(message)
+        except:
+            print("An Error occured")
+            client.close()
+            break
+
+
+def send():
+    while True:
+        message = f"{nickname} > {input()}"
+        client.send(message.encode(FORMAT))
+    # message = msg.encode(FORMAT)
+    # msg_length = len(message)
+    # send_length = str(msg_length).encode(FORMAT)
+    # send_length += b' ' * (HEADER-len(send_length))
+    # client.send(send_length)
+    # client.send(message)
     
 if __name__ == "__main__":
-    send("Hello, World!")
-    send("Hello, TEST!")
-    send("Hello, SERVER!")
-    send(DISCONNECT_MESSAGE)
+    receive_thread = threading.Thread(target=receive)
+    receive_thread.start()
+    
+    send_thread = threading.Thread(target=send)
+    send_thread.start()
--- server/server.py
+++ server/server.py
@@ -9,26 +9,39 @@ ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
+clients = []
+nicknames = []
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
 
 # Handle individual connections concurrently
-def handle_client(conn, addr):
-    print(f"Server > [NEW CONNECTION] {addr} connected.")
+def handle_client(client, address):
+    conn_message = f"Server > [NEW CONNECTION] {address} joined the world."
+    print(conn_message)
     
     connected = True
     while connected:
-        msg_length = conn.recv(HEADER).decode(FORMAT) #blocking line
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNECT_MESSAGE:
-                connected = False
-            
-            print(f"{addr} > {msg}")
-            conn.send("Msg received".encode())
+        try:
+            msg_length = client.recv(HEADER).decode(FORMAT) #blocking line
+            if msg_length:
+                msg_length = int(msg_length)
+                msg = client.recv(msg_length).decode(FORMAT)
+                if msg == DISCONNECT_MESSAGE:
+                    connected = False
+                
+                print(f"{address} > {msg}")
+                client.send("Msg received".encode(FORMAT))
+        except:
+            index = clients.index(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast(f"Server > {nickname} left the world.".encode(FORMAT))
+            nicknames.remove(nickname)
+            break
         
-    conn.close()
+    client.close()
 
 
 # Handle new connection
@@ -37,12 +50,25 @@ def start():
     print(f"Server > listening on {SERVER}")
     while True:
         # conn - socket allows us to communucate back connected client
-        # addr - info about the connection
-        conn, addr = server.accept() # bloacking line
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
-        thread.start()
-        print(f"Server > [ACTIVE CONNECTIONS] {threading.active_count()-1}")
+        # address - info about the connection
+        client, address = server.accept() # bloacking line
         
+        client.send("NICK".encode())
+        nickname = client.recv(1024).decode(FORMAT)
+        nicknames.append(nickname)
+        clients.append(client)
+        
+        print(f"Nickname of the client is {nickname}!")
+        broadcast(f"Server > {nickname} joined the world".encode())
+
+        thread = threading.Thread(target=handle_client, args=(client, address))
+        thread.start()
+        # print(f"Server > [ACTIVE CONNECTIONS] {threading.active_count()-1}")
+
+
+
 if __name__ == "__main__":
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(ADDR)
     print("Server > Sever is starting...")
     start()
